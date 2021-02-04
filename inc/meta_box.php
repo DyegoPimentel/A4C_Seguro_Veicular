37bd201201bec6da2d642a70575507f066f31978
Dyego Pimentel <dyegobusiness@gmail.com>
2021-02-04 00:02:51 -0300

    save and get meta box ok

--- inc/meta_box.php
+++ inc/meta_box.php
@@ -17,15 +17,15 @@ function A4C_add_meta_box() {
 function A4C_display_meta_box($post) {
     wp_nonce_field( basename( __FILE__ ), 'A4C_sv_nonce');
     $a4c_stored_meta = get_post_meta( $post->ID);
-
     ?>
+
     <div>
         <div class="meta-row">
             <div class="meta-th">
                 <label for="A4C-id-SV" class="a4c-row-title">ID</label>
             </div>
             <div class="meta-td">
-                <input type="text" name="A4C_id_SV" id="A4C-id-SV" value=""/>
+                <input type="text" name="A4C_id_SV" id="A4C-id-SV" value="<?php if ( ! empty ( $a4c_stored_meta['A4C_id_SV'] ) ) echo esc_attr( $a4c_stored_meta['A4C_id_SV'][0] ); ?>"/>
             </div>
         </div>
 
@@ -60,4 +60,24 @@ function A4C_display_meta_box($post) {
     
  <?php
 }
+
+function A4C_meta_save( $post_id ) {
+    // checks save status
+    $is_autosave = wp_is_post_autosave( $post_id );
+    $is_revision = wp_is_post_revision( $post_id );
+    $is_valid_nonce = ( isset( $_POST['A4C_sv_nonce'] ) && wp_verify_nonce( $_POST['A4C_sv_nonce'], basename( __FILE__ ))) ? 'true' : 'false';
+
+    // Exits script depending on save status
+    if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+        return;
+    }
+
+    if (isset( $_POST['A4C_id_SV'] )) {
+
+        update_post_meta($post_id, 'A4C_id_SV', sanitize_text_field( $_POST['A4C_id_SV'] ) );
+
+    }
+}
+add_action( 'save_post', 'A4C_meta_save');
+
 ?>
